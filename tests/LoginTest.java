b23aa7dc13b6352ec8a617b64a080b57bea7dd0a
Tharun Kumar <chikondratharunkumar>
2026-05-11 16:43:56 +0530

    Updated login validation messages

--- tests/LoginTest.java
+++ tests/LoginTest.java
@@ -2,6 +2,7 @@ public class LoginTest {
 
     public static void main(String[] args) {
 
-        System.out.println("Login Test Started");
+        System.out.println("Browser launched");
+        System.out.println("User logged in successfully");
     }
 }
